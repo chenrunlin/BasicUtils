91d235a16387453c9d7a1e7bb792bca76f600406
chenrunlin <chenrunlin516@163.com>
2018-01-23 18:08:27 +0800

    add httpUtil

---
+++ BasicUtils/src/main/java/com/bsit/http/RequestUtils.java
@@ -0,0 +1,144 @@
+package com.bsit.http;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class RequestUtils {
+    /**
+     * 发送post请求
+     *
+     * @param url  请求地址
+     * @param data 请求参数
+     * @return 返回请求信息
+     */
+    public static String sendPost(String url, String data) throws Exception {
+        URL localURL = new URL(url);
+        URLConnection connection = localURL.openConnection();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) connection;
+
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Accept-Charset", "utf-8");
+        httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        OutputStream outputStream = null;
+        OutputStreamWriter outputStreamWriter = null;
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader reader = null;
+        StringBuffer resultBuffer = new StringBuffer();
+        String tempLine = null;
+
+        try {
+            outputStream = httpURLConnection.getOutputStream();
+            outputStreamWriter = new OutputStreamWriter(outputStream);
+
+            outputStreamWriter.write(data);
+            outputStreamWriter.flush();
+
+            if (httpURLConnection.getResponseCode() >= 300) {
+                throw new Exception("HTTP Request is not success, Response code is "
+                        + httpURLConnection.getResponseCode());
+            }
+
+            inputStream = httpURLConnection.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream, "utf-8");
+            reader = new BufferedReader(inputStreamReader);
+
+            while ((tempLine = reader.readLine()) != null) {
+                resultBuffer.append(tempLine);
+            }
+        } finally {
+            if (outputStreamWriter != null) {
+                outputStreamWriter.close();
+            }
+
+            if (outputStream != null) {
+                outputStream.close();
+            }
+
+            if (reader != null) {
+                reader.close();
+            }
+
+            if (inputStreamReader != null) {
+                inputStreamReader.close();
+            }
+
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+        return resultBuffer.toString();
+    }
+
+
+    /**
+     * 发送get请求
+     *
+     * @param url   请求地址
+     * @param param 请求参数
+     * @return 返回请求信息
+     */
+    public static String sendGet(String url, String param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = "";
+            if (param.equals("")) {
+                urlNameString = url;
+            } else {
+                urlNameString = url + "?" + param;
+            }
+            URL realUrl = new URL(urlNameString);
+            URLConnection connection = realUrl.openConnection();
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("Accept-Charset", "utf-8");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.connect();
+            Map<String, List<String>> map = connection.getHeaderFields();
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    public static String getKeyAndValueStr(Map<String, String> map) {
+        String data = "";
+        for (Entry<String, String> e : map.entrySet()) {
+            String key = e.getKey();
+            String value = e.getValue();
+            data += key + "=" + value + "&";
+        }
+        return data.substring(0, data.length() - 1);
+    }
+}
---
+++ BasicUtils/src/main/java/com/bsit/http/ResponseUtils.java
@@ -0,0 +1,148 @@
+package com.bsit.http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+
+/**
+ * HttpServletResponse帮助类
+ * 
+ * 
+ */
+public final class ResponseUtils {
+	public static final Logger log = LoggerFactory.getLogger(ResponseUtils.class);
+
+	/**
+	 * 发送文本。使用UTF-8编码。
+	 * 
+	 * @param response
+	 *            HttpServletResponse
+	 * @param text
+	 *            发送的字符串
+	 */
+	public static void renderText(HttpServletResponse response, String text) {
+		render(response, "text/plain;charset=UTF-8", text);
+	}
+
+	/**
+	 * 发送文本。使用UTF-8编码。
+	 * 
+	 * @param response
+	 *            HttpServletResponse
+	 * @param text
+	 *            发送的字符串
+	 */
+	public static void renderHtml(HttpServletResponse response, String text) {
+		render(response, "text/html;charset=UTF-8", text);
+	}
+
+	/**
+	 * 发送json。使用UTF-8编码。
+	 * 
+	 * @param response
+	 *            HttpServletResponse
+	 * @param text
+	 *            发送的字符串
+	 */
+	public static void renderJson(HttpServletResponse response, String text) {
+		//System.out.print(text);
+		render(response, "text/plain;charset=UTF-8", text);
+	}
+
+	/**
+	 * 发送json。使用UTF-8编码。
+	 * 
+	 * @param response
+	 *            HttpServletResponse
+	 *            发送的字符串
+	 */
+	public static void renderJsonWithJsonCallBack(HttpServletResponse response, String responseText, String jsoncallbackString) {
+		if (jsoncallbackString != null && jsoncallbackString.length() > 0) {
+			String jsonp = StringUtils.hasText(jsoncallbackString) ? jsoncallbackString + "(" + responseText + ")" : responseText;
+			render(response, "text/plain;charset=UTF-8", jsonp);
+		} else {
+			render(response, "text/plain;charset=UTF-8", responseText);
+		}
+	}
+
+	/**
+	 * 发送xml。使用UTF-8编码。
+	 * 
+	 * @param response
+	 *            HttpServletResponse
+	 * @param text
+	 *            发送的字符串
+	 */
+	public static void renderXml(HttpServletResponse response, String text) {
+		render(response, "text/xml;charset=UTF-8", text);
+	}
+
+	/**
+	 * 发送内容。使用UTF-8编码。
+	 * 
+	 * @param response
+	 * @param contentType
+	 * @param text
+	 */
+	public static void render(HttpServletResponse response, String contentType, String text) {
+		response.setContentType(contentType);
+		response.setHeader("Pragma", "No-cache");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setDateHeader("Expires", 0);
+		try {
+			response.getWriter().write(text);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+
+	public static void renderImage(HttpServletResponse response, InputStream inputStream) throws IOException {
+		response.setContentType("image/jpeg");
+
+		response.setHeader("Pragma", "No-cache");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setDateHeader("Expires", 0);
+
+		OutputStream outputStream = response.getOutputStream();
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while ((len = inputStream.read(buffer)) > 0) {
+			outputStream.write(buffer, 0, len);
+		}
+	}
+
+	public static void renderExcel(HttpServletResponse response, File file, String fileName) throws IOException {
+		response.setContentType("application/msexcel;charset=utf-8");
+		response.setCharacterEncoding("utf-8");
+		// 文件名
+		response.addHeader("Content-Disposition", "attachment;filename=\"" + fileName + "\"");
+		response.setContentLength((int) file.length());
+		byte[] buffer = new byte[4096];// 缓冲区
+		BufferedOutputStream output = null;
+		BufferedInputStream input = null;
+		try {
+			output = new BufferedOutputStream(response.getOutputStream());
+			input = new BufferedInputStream(new FileInputStream(file));
+			int n = -1;
+			// 遍历，开始下载
+			while ((n = input.read(buffer, 0, 4096)) > -1) {
+				output.write(buffer, 0, n);
+			}
+			output.flush(); // 不可少
+			response.flushBuffer();// 不可少
+		} catch (Exception e) {
+			// 异常自己捕捉
+		} finally {
+			// 关闭流，不可少
+			if (input != null)
+				input.close();
+			if (output != null)
+				output.close();
+		}
+	}
+}
